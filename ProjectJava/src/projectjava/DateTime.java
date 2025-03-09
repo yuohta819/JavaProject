058f8ae071119b5c3216cd18ddda2d926db81609
unknown <hannh11425@gmail.com>
2025-03-09 22:29:26 +0700

    add class file

---
+++ ProjectJava/src/projectjava/DateTime.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectjava;
+
+/**
+ *
+ * @author Nguyen Hoang Han - CE192048
+ */
+public interface DateTime {
+    
+}
