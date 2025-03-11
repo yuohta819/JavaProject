d3a080e22407e18b68fc31e1b74c77140da3e975
yuohta819 <thunhan798@gmail.com>
2025-03-11 16:11:49 +0700

    123

--- ProjectJava/src/projectjava/ProjectJava.java
+++ ProjectJava/src/projectjava/ProjectJava.java
@@ -15,7 +15,7 @@ public class ProjectJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
     }
     
 }
